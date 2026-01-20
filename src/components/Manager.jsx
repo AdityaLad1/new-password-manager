cb9ff580755a76c35399edf482cc0f0bf41d291b
Aditya Lad <adityanlad@gmail.com>
2026-01-20 16:14:06 +0530

    password table and github icon on navbar

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,56 +1,50 @@
-import React, { useState, useRef,useEffect  } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import eye from "../assets/eye.png";
 import hiddeneye from "../assets/hidden.png";
 
 const Managger = () => {
   const ref = useRef();
   const [eyeimage, setEyeimage] = useState(true);
-  const [form, setForm] = useState({site:"",username:"",password:""})
-  const [passwordArray, setPasswordArray] = useState([])
+  const [form, setForm] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setPasswordArray] = useState([]);
 
   useEffect(() => {
-    let passwords = localStorage.getItem("passwords")
-   
-    if(passwords){
-        setPasswordArray(JSON.parse(passwords))
+    let passwords = localStorage.getItem("passwords");
+
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords));
     }
     // else{
     //     passwordArray=[]
     // }
-  
-    
-  }, [])
-  
+  }, []);
 
-    const showPassword = () => {
-      alert("showpassword");
+  const showPassword = () => {
+    alert("showpassword");
     //   setEyeimage();
     //   ref.current.src = "src/assets/hidden.png";
-    };
-    const savePassword = () => {
-    setPasswordArray([...passwordArray,form])
-    localStorage.setItem("passwords",JSON.stringify([...passwordArray,form]))
+  };
+  const savePassword = () => {
+    setPasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
     console.log(passwordArray);
-    
-
-  setForm({ site: "", username: "", password: "" });
-    };
-    const handleChange = (e) => {
-      setForm({...form,[e.target.name]:e.target.value})
-   
-    };
+    setForm({ site: "", username: "", password: "" });
+  };
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   return (
     <div>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-size-[14px_24px]">
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-77.5 w-77.5 rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
       </div>
       <div className=" mycontainer">
-        <h1 className="text-4xl text font-bold text-center">
+        <h1 className="text-4xl text font-bold text-center py-2 border border-white w-32">
           <span className="text-green-500">&lt;</span>
           Password-Manager
           <span className="text-green-500">OP/&gt;</span>
         </h1>
-        <p className="text-green-900 text-center text-lg">
+        <p className="text-green-900 text-center py-2 border border-white w-32 text-lg">
           Your Password Manger
         </p>
 
@@ -99,13 +93,51 @@ const Managger = () => {
             </div>
           </div>
 
-          <button onClick={savePassword} className="flex justify-center items-center gap-2 bg-green-600 hover:bg-green-500 rounded-full px-8 py-2 w-fit">
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center gap-2 bg-green-600 hover:bg-green-500 rounded-full px-8 py-2 w-fit"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/ueoydrft.json"
               trigger="hover"
             ></lord-icon>
             Add Password
           </button>
+        </div>
+        <div className="passwords">
+          <h2 className="font-bold text-2xl py-4">Your password</h2>
+          {passwordArray.length === 0 && <div>No passwords to show</div>}
+          {passwordArray.length != 0 && (
+            <table className="table-auto w-full rounded-md overflow-hidden">
+              <thead className="bg-green-800 text-white ">
+                <tr>
+                  <th className="py-2">Site</th>
+                  <th className="py-2">Username</th>
+                  <th className="py-2">Passwords</th>
+                </tr>
+              </thead>
+              <tbody className="bg-green-100">
+                {passwordArray.map((items, index) => {
+                  return (
+                    <tr key={index}>
+                      <td className="text-center py-2 border border-white w-32">
+                        <a href={items.site} target="_blank">
+
+                        {items.site}
+                        </a>
+                      </td>
+                      <td className="text-center py-2 border border-white w-32">
+                        {items.username}
+                      </td>
+                      <td className="text-center py-2 border border-white w-32">
+                        {items.password}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,23 @@
 import React from "react";
+import github from "../assets/github.svg";
 
 const Navbar = () => {
   return (
- 
-   
     <nav className="bg-emerald-950 text-white font-bold">
       <div className="mycontainer flex justify-between items-center px-4 h-14 py-5 ">
-
-      <div className="logo text-2xl">
-        <span className="text-green-500">&lt;</span>
-        
-        Password-Manager
-        <span className="text-green-500">OP/&gt;</span>
+        <div className="logo text-2xl">
+          <span className="text-green-500">&lt;</span>
+          Password-Manager
+          <span className="text-green-500">OP/&gt;</span>
+        </div>
+        <a  target="_blank"
+  rel="noopener noreferrer" href="https://github.com/AdityaLad1">
+          <button className="cursor-pointer text-white bg-emerald-900 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1">
+            <img className="invert  w-10 p-1" src={github} alt="github logo" />
+            <span className="font-bold px-2">GitHub</span>
+          </button>
+        </a>
       </div>
-      <ul>
-        <li className="flex gap-4">
-          <a href="/" className="hover:font-bold">Home</a>
-          <a href="#" className="hover:font-bold">About</a>
-          <a href="#" className="hover:font-bold">Contact</a>
-        </li>
-      </ul>
-      </div>
-
     </nav>
   );
 };
