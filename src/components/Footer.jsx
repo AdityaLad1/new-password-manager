981e96cbf6c42acbdec4ae3c1dc8b63ebf9376c9
Aditya Lad <adityanlad@gmail.com>
2026-01-20 16:22:29 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="bg-emerald-950 text-white flex flex-col justify-center items-center  w-full">
+      <div className="logo font-bold text-white text-2xl">
+        <span className="text-green-500"> &lt;</span>
 
-export default Footer
+        <span>Password-Manager</span>
+        <span className="text-green-500">OP/&gt;</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,11 @@ const Navbar = () => {
           Password-Manager
           <span className="text-green-500">OP/&gt;</span>
         </div>
-        <a  target="_blank"
-  rel="noopener noreferrer" href="https://github.com/AdityaLad1">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"//prevents security vulnerabilities when opening external links in a new tab by blocking access to window.opener and hiding referrer information
+          href="https://github.com/AdityaLad1"
+        >
           <button className="cursor-pointer text-white bg-emerald-900 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1">
             <img className="invert  w-10 p-1" src={github} alt="github logo" />
             <span className="font-bold px-2">GitHub</span>
